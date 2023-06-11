f170d0cae11b84530f268b7b88c7096fad17f140
razeeth11 <razeeth011@gmail.com>
2023-06-11 12:09:10 +0530

    Table-UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,39 @@ import "./App.css";
 
 function App() {
   const [dataTable, setDataTable] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true)
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((data) => data.json())
       .then((res) => setDataTable(res));
+       setLoading(false);
   }, []);
 
-  console.log(dataTable);
+  // console.log(dataTable);
   return (
     <div className="App">
-      <table>
-        <thead>
-          <tr>
-            <th>USER_ID</th>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Body</th>
-          </tr>
-        </thead>
-        {dataTable.map((data, index) => (
-          <tbody key={index}>
+        <table>
+          <thead>
             <tr>
-              <td>{data.userId}</td>
-              <td>{data.id}</td>
-              <td>{data.title}</td>
-              <td>{data.body}</td>
+              <th>USER_ID</th>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
             </tr>
-          </tbody>
-        ))}
-      </table>
+          </thead>
+          {dataTable.map((data, index) => (
+            <tbody key={index}>
+              <tr>
+                <td>{data.userId}</td>
+                <td>{data.id}</td>
+                <td>{data.title}</td>
+                <td>{data.body}</td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
     </div>
   );
 }
