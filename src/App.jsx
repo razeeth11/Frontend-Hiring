b79a7cb77690a1eda8f0a5bb1a65eb101f94cd8c
razeeth11 <razeeth011@gmail.com>
2023-06-11 11:46:18 +0530

    Table-Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [dataTable, setDataTable] = useState([]);
 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => data.json())
+      .then((res) => setDataTable(res));
+  }, []);
+
+  console.log(dataTable);
   return (
-    <div className='App'>
-
+    <div className="App">
+      <table>
+        <thead>
+          <tr>
+            <th>USER_ID</th>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        {dataTable.map((data, index) => (
+          <tbody key={index}>
+            <tr>
+              <td>{data.userId}</td>
+              <td>{data.id}</td>
+              <td>{data.title}</td>
+              <td>{data.body}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
