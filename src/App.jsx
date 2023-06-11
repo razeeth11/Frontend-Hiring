10cd6dde350dba376fac2cb00b88a161d2fae1ca
razeeth11 <razeeth011@gmail.com>
2023-06-11 15:47:15 +0530

    search-filter-UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,46 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  return (
+    <div className="App">
+      <DataTable />
+    </div>
+  );
+}
+
+function DataTable() {
   const [dataTable, setDataTable] = useState([]);
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true)
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((data) => data.json())
       .then((res) => setDataTable(res));
-       setLoading(false);
   }, []);
 
   // console.log(dataTable);
   return (
-    <div className="App">
-        <table>
-          <thead>
+    <div className="Data-table">
+      <input type="text" placeholder="Search....." /> 
+      <table>
+        <thead>
+          <tr>
+            <th>USER_ID</th>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        {dataTable.map((data, index) => (
+          <tbody key={index}>
             <tr>
-              <th>USER_ID</th>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Body</th>
+              <td>{data.userId}</td>
+              <td>{data.id}</td>
+              <td>{data.title}</td>
+              <td>{data.body}</td>
             </tr>
-          </thead>
-          {dataTable.map((data, index) => (
-            <tbody key={index}>
-              <tr>
-                <td>{data.userId}</td>
-                <td>{data.id}</td>
-                <td>{data.title}</td>
-                <td>{data.body}</td>
-              </tr>
-            </tbody>
-          ))}
-        </table>
+          </tbody>
+        ))}
+      </table>
     </div>
   );
 }
